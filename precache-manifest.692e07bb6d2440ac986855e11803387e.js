8b99478588281065625468a958e4ece383574892
Jordan Ward <jw363912@ohio.edu>
2020-01-02 21:01:54 -0500

    Updates

---
+++ precache-manifest.692e07bb6d2440ac986855e11803387e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef15bf8695e20c66b06d0ce80f41bc7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62d0d9a7bb7e9c61a477",
+    "url": "/monsters-rolodex/static/css/main.5c979841.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62d0d9a7bb7e9c61a477",
+    "url": "/monsters-rolodex/static/js/main.ab7b42f9.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
